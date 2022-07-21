e522241252aa6997014111494f8aee01efd61c93
Kevin Malakoff <kmalakoff@gmail.com>
2022-07-20 21:14:31 -0700

    Restructure library

--- index.js
+++
@@ -1,9 +0,0 @@
-var asyncFunction = require('./lib/asyncFunction');
-var asyncValue = require('./lib/asyncValue');
-var defaultValue = require('./lib/defaultValue');
-
-module.exports = {
-  asyncFunction: asyncFunction,
-  asyncValue: asyncValue,
-  defaultValue: defaultValue,
-};
