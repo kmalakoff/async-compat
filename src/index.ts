ddeed93cdfffcb9b1bf9d2260a6fe979bb138750
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-01 21:15:29 -0800

    Upgrade to typescript

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export * from './types';
+export { default as asyncFunction } from './asyncFunction';
+export { default as asyncValue } from './asyncValue';
+export { default as defaultValue } from './defaultValue';
+
+import { default as asyncFunction } from './asyncFunction';
+import { default as asyncValue } from './asyncValue';
+import { default as defaultValue } from './defaultValue';
+
+export default { asyncFunction, asyncValue, defaultValue };
