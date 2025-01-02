602e5d17b049cb44883f924fd8e032895ff62000
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-01 20:16:19 -0800

    Migrate to TypeScript and Biome

--- test/spec/asyncAwait.js
+++ test/spec/asyncAwait.js
@@ -1,15 +1,28 @@
-var assert = require('assert');
+const assert = require('assert');
 
-var compat = require('../..');
+const compat = require('../..');
 
-describe('async await', function () {
-  it('one argument', function (done) {
+describe('async await', () => {
+  (() => {
+    // patch and restore promise
+    const root = typeof global !== 'undefined' ? global : window;
+    let rootPromise;
+    before(() => {
+      rootPromise = root.Promise;
+      root.Promise = require('pinkie-promise');
+    });
+    after(() => {
+      root.Promise = rootPromise;
+    });
+  })();
+
+  it('one argument', (done) => {
     async function testFn(arg1) {
       assert.equal(arg1, 1);
       return true;
     }
-    compat.asyncFunction(testFn, false, 1, function (err, result) {
-      assert.ok(!err);
+    compat.asyncFunction(testFn, false, 1, (err, result) => {
+      assert.ok(!err, err ? err.message : '');
       assert.equal(result, true);
       done();
     });
