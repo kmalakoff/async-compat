86e1bed0bb1367d72a88ab3e516fd3a9b90a05b7
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-31 11:58:56 -0700

    Comprehensive types

--- src/types.ts
+++ src/types.ts
@@ -8,12 +8,12 @@ export type AsyncCallbackFn4 = (arg1: unknown, arg2: unknown, arg3: unknown, arg
 export type AsyncCallbackFn5 = (arg1: unknown, arg2: unknown, arg3: unknown, arg4: unknown, arg5: unknown, callback: AsyncCallback) => unknown;
 export type AsyncCallbackFn6 = (arg1: unknown, arg2: unknown, arg3: unknown, arg4: unknown, arg5: unknown, arg6: unknown, callback: AsyncCallback) => unknown;
 
-export type AsyncPromiseFn0 = () => Promise<unknown>;
-export type AsyncPromiseFn1 = (arg1: unknown) => Promise<unknown>;
-export type AsyncPromiseFn2 = (arg1: unknown, arg2: unknown) => Promise<unknown>;
-export type AsyncPromiseFn3 = (arg1: unknown, arg2: unknown, arg3: unknown) => Promise<unknown>;
-export type AsyncPromiseFn4 = (arg1: unknown, arg2: unknown, arg3: unknown, arg4: unknown) => Promise<unknown>;
-export type AsyncPromiseFn5 = (arg1: unknown, arg2: unknown, arg3: unknown, arg4: unknown, arg5: unknown) => Promise<unknown>;
-export type AsyncPromiseFn6 = (arg1: unknown, arg2: unknown, arg3: unknown, arg4: unknown, arg5: unknown, arg6: unknown) => Promise<unknown>;
+export type AsyncFn0 = () => unknown | Promise<unknown>;
+export type AsyncFn1 = (arg1: unknown) => unknown | Promise<unknown>;
+export type AsyncFn2 = (arg1: unknown, arg2: unknown) => unknown | Promise<unknown>;
+export type AsyncFn3 = (arg1: unknown, arg2: unknown, arg3: unknown) => unknown | Promise<unknown>;
+export type AsyncFn4 = (arg1: unknown, arg2: unknown, arg3: unknown, arg4: unknown) => unknown | Promise<unknown>;
+export type AsyncFn5 = (arg1: unknown, arg2: unknown, arg3: unknown, arg4: unknown, arg5: unknown) => unknown | Promise<unknown>;
+export type AsyncFn6 = (arg1: unknown, arg2: unknown, arg3: unknown, arg4: unknown, arg5: unknown, arg6: unknown) => unknown | Promise<unknown>;
 
-export type AsyncFunction = AsyncCallbackFn0 | AsyncCallbackFn1 | AsyncCallbackFn2 | AsyncCallbackFn3 | AsyncCallbackFn4 | AsyncCallbackFn5 | AsyncCallbackFn6 | AsyncPromiseFn0 | AsyncPromiseFn1 | AsyncPromiseFn2 | AsyncPromiseFn3 | AsyncPromiseFn4 | AsyncPromiseFn5 | AsyncPromiseFn6;
+export type AsyncFunction = AsyncCallbackFn0 | AsyncCallbackFn1 | AsyncCallbackFn2 | AsyncCallbackFn3 | AsyncCallbackFn4 | AsyncCallbackFn5 | AsyncCallbackFn6 | AsyncFn0 | AsyncFn1 | AsyncFn2 | AsyncFn3 | AsyncFn4 | AsyncFn5 | AsyncFn6;
