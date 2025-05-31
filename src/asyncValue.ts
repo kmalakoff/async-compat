79d5cfe3b3dbafc576e50655eef53d8bd6880dcb
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-31 09:48:45 -0700

    Comprehensive types

--- src/asyncValue.ts
+++ src/asyncValue.ts
@@ -1,8 +1,10 @@
 import isError from 'is-error';
 import isPromise from 'is-promise';
 
-export default function asyncValue(value, callback) {
-  if (isError(value)) return callback(value);
+import type { AsyncCallback } from './types.js';
+
+export default function asyncValue(value: unknown, callback: AsyncCallback): undefined | unknown {
+  if (isError(value)) return callback(value as Error);
   if (isPromise(value)) {
     return (value as Promise<unknown>)
       .then((result) => {
