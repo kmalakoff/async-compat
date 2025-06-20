23fc570ad77467e92c2690ba510b2bcc29d9ebbc
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-19 23:22:00 -0700

    Switch to mocha for running tests

--- test/spec/asyncAwait.test.ts
+++ test/spec/asyncAwait.test.ts
@@ -6,14 +6,13 @@ import Pinkie from 'pinkie-promise';
 describe('async await', () => {
   (() => {
     // patch and restore promise
-    // @ts-ignore
-    let rootPromise: Promise;
+    if (typeof global === 'undefined') return;
+    const globalPromise = global.Promise;
     before(() => {
-      rootPromise = global.Promise;
       global.Promise = Pinkie;
     });
     after(() => {
-      global.Promise = rootPromise;
+      global.Promise = globalPromise;
     });
   })();
 
