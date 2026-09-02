5d29b46563bf15e996ca3b31715f7e1b48c02184
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-01 23:17:07 -0700

    Configure CI, VS Code debugging, and update dependencies

--- test/spec/platform-specific.test.js
+++ test/spec/platform-specific.test.js
@@ -1,5 +1,3 @@
 const HAS_ASYNC_AWAIT = typeof Symbol !== 'undefined' && Symbol.asyncIterator;
 
-describe('platform specific', function () {
-  if (HAS_ASYNC_AWAIT) require('./asyncAwait');
-});
+!HAS_ASYNC_AWAIT || require('./asyncAwait');
